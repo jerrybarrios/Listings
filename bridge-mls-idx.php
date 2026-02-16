016e6572c2e67ef9a145b3288a28feb06b46a36d
jerrybarrios <info@jerrybarrios.com>
2026-02-15 23:06:19 -0500

    Create main plugin file for Bridge MLS IDX

---
+++ bridge-mls-idx.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Plugin Name: Bridge MLS IDX
+ * Description: A plugin to integrate MLS listings using Bridge Interactive API.
+ * Version: 1.0.0
+ * Author: Jerry Barrios
+ */
+
+// Initialize the plugin
+function bridge_mls_idj_init() {
+    // Plugin code goes here
+}
+add_action('plugins_loaded', 'bridge_mls_idj_init');
